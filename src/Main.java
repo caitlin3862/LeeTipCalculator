c8d7791b27efba5785eb6dcd70487106eec0a89d
BT_2N2_04 <sample@bths.edu>
2023-09-21 13:10:43 -0400

    Began my code for the tip calculator

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to the tip calculator!");
+        System.out.println("How many people are in your group? ");
+        int customers = scan.nextInt();
+
+
+        double cost = 0.0;
+        while (cost != -1) {
+            System.out.println();
+        }
+
+
+        }
+    }
