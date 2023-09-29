0a5ac9bd399c7d62cd11a306aeb6110df63f19a0
Caitlin Lee <caitlinlee@Caitlins-MacBook-Air.local>
2023-09-29 14:00:59 -0400

    Added item list, but not finished

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,18 @@ public class Main {
         int customers = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
         int percent = scan.nextInt();
-
         System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
         double cost = scan.nextDouble();
         System.out.print("Enter the item: ");
         String item = scan.nextLine();
+        System.out.println();
         double totalCost1 = cost;
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
             System.out.print("Enter the item: ");
             item = scan.nextLine();
+            System.out.println();
             if (cost != -1) {
                 totalCost1 = totalCost1 + cost;
             } else {
@@ -32,20 +33,26 @@ public class Main {
         System.out.println("Total percentage: " + percent + "%");
 
         double tip = totalCost1*((double)percent / 100);
-
-        System.out.println("Total tip: $" + tip);
+        double tip2 = Math.round(tip * 100.0) / 100.0;
+        System.out.println("Total tip: $" + tip2);
 
         double finalTip = totalCost1 + tip;
-        System.out.println("Total bill with tip: $" + finalTip);
+        double finalTip2 = Math.round(finalTip * 100.0) / 100.0;
+        System.out.println("Total bill with tip: $" + finalTip2);
 
         double personNoTip = totalCost1 / customers;
-        System.out.println("Per person cost before tip: $" + personNoTip);
+        double personNoTip2 = Math.round(personNoTip * 100.0) / 100.0;
+        System.out.println("Per person cost before tip: $" + personNoTip2);
 
         double personTip = tip / customers;
-        System.out.println("Tip per person: $" + personTip);
+        double personTip2 = Math.round(personTip * 100.0) / 100.0;
+        System.out.println("Tip per person: $" + personTip2);
 
         double costPerPerson = personTip + personNoTip;
-        System.out.println("Total cost per person: $"+ costPerPerson);
+        double costPerPerson2 = Math.round(costPerPerson * 100.0) / 100.0;
+        System.out.println("Total cost per person: $"+ costPerPerson2);
+
+
 
             }
         }
